506e71cbfed1cdd2589f29c07fb4d26bdfa6c1ee
CipolatFrancesca <57095576+CipolatFrancesca@users.noreply.github.com>
2019-11-04 13:01:26 +0100

    Add files via upload

---
+++ informatica/03_latteria/3latteria.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inf.cipolatf2709
+ * Date: 30/10/2019
+ * Time: 13:06
+ */
+    $latteScelto=$_REQUEST["latte"];
+echo "ordinazione:" ;
+echo "{\"latte\":".$latteScelto."}";
+
+?>
