7636163515716039bbf34dd9553cafd20287914e
CipolatFrancesca <57095576+CipolatFrancesca@users.noreply.github.com>
2020-01-30 15:06:37 +0100

    Add files via upload

---
+++ informatica/17_computer/jsonComputer.js
@@ -0,0 +1,5 @@
+[{"id":"001", "marcaTastiera":"Asus","marcaVideo":"Asus", "marcaCase":"Asus"},
+{"id":"002", "marcaTastiera":"Apple","marcaVideo":"Apple", "marcaCase":"Apple"},
+{"id":"003", "marcaTastiera":"Huawei","marcaVideo":"Huawei", "marcaCase":"Huawei"},
+{"id":"004", "marcaTastiera":"Lenovo","marcaVideo":"Lenovo", "marcaCase":"Lenovo"}]
+	
